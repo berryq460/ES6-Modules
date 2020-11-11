7db6ae56f106fc1c4191319437e8a7575a2b44aa
Quiana Berry <berry.quiana@gmail.com>
2020-11-10 23:58:54 -0500

    Update issues.js

--- ES6practicalguide/components-module/issues.js
+++ ES6practicalguide/components-module/issues.js
@@ -3,4 +3,4 @@
 // Dynamic interface
 export { users } from './users';
 export { issues } from './issues';
-export { userProfile } from './user-profile';
+export { userProfile } from './user-profile.js';
